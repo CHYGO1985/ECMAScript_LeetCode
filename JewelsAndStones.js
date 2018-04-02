67d40270dc6460bf595f43a8fd235a2ff5007cd5
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-04-02 10:00:06 +0800

    Set is not suitable, change to array

--- JewelsAndStones.js
+++ JewelsAndStones.js
@@ -1,2 +1,15 @@
-const msg = 'Hello World!';
-console.log(`>>>>>>>>> First: ${msg}`);
+// const j = 'aA';
+// const s = 'aAAbbbb';
+
+const j = 'z';
+const s = 'ZZ';
+
+const numJewelsInStones = function (J, S) {
+  const jewels = new Set(Array.from(J));
+  const stones = new Set(Array.from(S));
+  const res = new Set([...jewels].filter(ele => stones.has(ele)));
+  return res.size;
+};
+
+console.log(numJewelsInStones(j, s));
+
