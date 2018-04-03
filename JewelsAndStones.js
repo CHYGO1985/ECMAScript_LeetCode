05599e16e56259b56beebfb781eed70399ae10f9
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-04-03 08:58:22 +0800

    771 passed version

--- JewelsAndStones.js
+++ JewelsAndStones.js
@@ -6,9 +6,9 @@ const s = 'ZZ';
 
 const numJewelsInStones = function (J, S) {
   const jewels = new Set(Array.from(J));
-  const stones = new Set(Array.from(S));
-  const res = new Set([...jewels].filter(ele => stones.has(ele)));
-  return res.size;
+  const stones = Array.from(S);
+  const res = [...stones].filter(ele => jewels.has(ele));
+  return res.length;
 };
 
 console.log(numJewelsInStones(j, s));
