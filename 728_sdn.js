421a88f9311a75cd00af8bc43ec2b97fbb9b1756
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-04-09 09:50:46 +0800

    728. Self Dividing Numbers: method1

--- 728_sdn.js
+++ 728_sdn.js
@@ -5,28 +5,28 @@
 const L = 1;
 const R = 22;
 
-// const checker = function (num) {
-//   let temp = num;
-//   while (Math.trunc(temp / 10) !== 0) {
-//     if ((num % 10 === 0) || (num % Math.trunc(temp % 10) !== 0)) {
-//       return false;
-//     }
-//     temp = Math.trunc(temp / 10);
-//   }
-//   return num % temp === 0;
-// };
+const checker = function (num) {
+  let temp = num;
+  while (Math.trunc(temp / 10) !== 0) {
+    if ((num % 10 === 0) || (num % Math.trunc(temp % 10) !== 0)) {
+      return false;
+    }
+    temp = Math.trunc(temp / 10);
+  }
+  return num % temp === 0;
+};
 
-// const selfDividingNumbers = function (left, right) {
-//   const res = [];
-//   for (let i = left; i <= right; i += 1) {
-//     if (checker(i) === true) {
-//       res.push(i);
-//     }
-//   }
-//   return res;
-// };
+const selfDividingNumbers = function (left, right) {
+  const res = [];
+  for (let i = left; i <= right; i += 1) {
+    if (checker(i) === true) {
+      res.push(i);
+    }
+  }
+  return res;
+};
 
-// console.log(selfDividingNumbers(L, R));
+console.log(selfDividingNumbers(L, R));
 
 // method 2:
 // ref: https://leetcode.com/problems/self-dividing-numbers/discuss/109394/My-Javascript-Solution
