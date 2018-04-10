32ac047f9cd697dec83aa8db77d9f09e6c8ff1a8
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-04-10 13:53:04 +0800

    806. Number of Lines To Write String

--- 806_NoLtWS.js
+++ 806_NoLtWS.js
@@ -1,15 +1,35 @@
-const w = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
-const s = 'bbbcccdddaaa';
+// const w = [4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+//   10, 10, 10, 10, 10, 10];
+// const s = 'bbbcccdddaaa';
 
+const w = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+  10, 10, 10, 10, 10, 10];
+const s = 'abcdefghijklmnopqrstuvwxyz';
+
+/**
+ * solved.
+ * @author jingjiejiang Apr 10, 2018 
+ */
 const getWidthVal = function (ele) {
   const base = 'a'.charCodeAt(0);
-  return ele.charCodeAt(0) - base; 
-}
+  return ele.charCodeAt(0) - base;
+};
 
-//TODO: use map + reduce
 const numberOfLines = function (widths, S) {
-  const 
-  [...S].map(ele => widths[getWidthVal(ele)]).reduce;    
+  let count = S.length === 0 ? 0 : 1;
+  const lastWidth = [...S].map(ele => widths[getWidthVal(ele)])
+    .reduce((preVal, curVal) => {
+      if (curVal + preVal > 100) {
+        count += 1;
+        preVal = curVal;
+      } else {
+        curVal += preVal;
+      }
+
+      return curVal;
+    });
+
+  return [count, lastWidth];
 };
 
 console.log(numberOfLines(w, s));
