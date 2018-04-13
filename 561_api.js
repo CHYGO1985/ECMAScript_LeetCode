d0af675892d6faf7cce85b1178d1c315d4d3df2f
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-04-13 23:59:11 +0800

    561 add test

--- 561_api.js
+++ 561_api.js
@@ -3,12 +3,10 @@
  * @author jingjie jiang Apr 11, 2018
  */
 // const arr = [1, 4, 2, 3, 5, 6];
-const arr = [-470, 66, -4835, -5623];
+const arr = [-470, 66, -4835, -5623，-1];
 
 const arrayPairSum = function (nums) {
-  nums.sort((a, b) => {
-    return a - b;
-  });
+  nums.sort((a, b) => a - b);
   const res = nums.reduce((acc, cur, index) => {
     if (index > 0 && index % 2 !== 0) {
       return acc + Math.min(cur, nums[index - 1]);
