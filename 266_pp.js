ccf9a1c5e953a31b19c28594bfa5b379948775fa
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-04-27 09:48:47 +0800

    266. Palindrome Permutation: incomplete

---
+++ 266_pp.js
@@ -0,0 +1,8 @@
+const canPermutePalindrome = function (s) {
+  // use an 26 length array to store the result
+  // conver to lowercase
+  // if the sum of the array <= 1, then it is a palindrome string
+  const res = [];
+  s = s.toLowerCase();
+  s.forEach();
+};
