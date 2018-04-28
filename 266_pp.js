4ad60f2ce31dad1e0a2833f8988aff6ce365021c
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-04-28 09:43:08 +0800

    266. Palindrome Permutation: need to consider non-lowercase

--- 266_pp.js
+++ 266_pp.js
@@ -1,8 +1,21 @@
+// const word = 'code';
+// const word = 'carerac';
+const word = '"AaBb//a"';
+
 const canPermutePalindrome = function (s) {
   // use an 26 length array to store the result
   // conver to lowercase
   // if the sum of the array <= 1, then it is a palindrome string
   const res = [];
-  s = s.toLowerCase();
-  s.forEach();
+  const arr = s.split('');
+  arr.forEach((ele) => {
+    const index = ele.charCodeAt(0) - 'a'.charCodeAt(0);
+    if (!res[index]) res[index] = 0;
+    res[index] += 1;
+  });
+  console.log(res);
+  const count = res.reduce(((acc, cur) => acc += (cur % 2)), 0);
+  return count <= 1;
 };
+
+console.log(canPermutePalindrome(word));
