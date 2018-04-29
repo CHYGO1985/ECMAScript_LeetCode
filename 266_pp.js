9c02615cf0a064e6d10196eeca6c3118cb08267b
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-04-29 16:36:36 +0800

    266. Palindrome Permutation

--- 266_pp.js
+++ 266_pp.js
@@ -1,20 +1,21 @@
-// const word = 'code';
+/**
+ *
+ * @author jingjiejiang Apr 29, 2018
+ */
+const word = 'code';
 // const word = 'carerac';
-const word = '"AaBb//a"';
+// const word = 'AaBb//a';
 
 const canPermutePalindrome = function (s) {
   // use an 26 length array to store the result
   // conver to lowercase
   // if the sum of the array <= 1, then it is a palindrome string
   const res = [];
-  const arr = s.split('');
-  arr.forEach((ele) => {
-    const index = ele.charCodeAt(0) - 'a'.charCodeAt(0);
-    if (!res[index]) res[index] = 0;
-    res[index] += 1;
+  s.split('').forEach((ele) => {
+    if (!res[ele]) res[ele] = 0;
+    res[ele] += 1;
   });
-  console.log(res);
-  const count = res.reduce(((acc, cur) => acc += (cur % 2)), 0);
+  const count = Object.values(res).reduce(((acc, cur) => acc += (cur % 2)), 0);
   return count <= 1;
 };
 
