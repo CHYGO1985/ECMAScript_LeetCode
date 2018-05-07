3e81bd536d2c7de18105a84afc05eeffa0224375
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-05-07 22:00:58 +0800

    746. Min Cost Climbing Stairs

---
+++ 746_mccs.js
@@ -0,0 +1,17 @@
+const params = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+const minCostClimbingStairs = function (cost) {
+  // p(i) = cost[i - 1] + res[i - 1],  cost[i - 2] + res[i - 2];
+  const res = new Array(cost.length + 1);
+  res[0] = 0;
+  res[1] = 0;
+  for (let i = 2; i < res.length; i += 1) {
+    res[i] = Math.min(res[i - 1] + cost[i - 1], res[i - 2] + cost[i - 2]);
+  }
+  return res[cost.length];
+};
+
+console.log(minCostClimbingStairs(params));
