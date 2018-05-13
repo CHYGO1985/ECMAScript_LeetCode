6ad64dab409c756bb4d5f883049bcc3f1e75dfff
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-05-13 17:06:44 +0800

    revise test

--- promise/promise_all.js
+++ promise/promise_all.js
@@ -6,48 +6,19 @@
 const nums = [1, 2, 3, 4, 5];
 
 /* test with async and await */
-// const getPromises = function (params) {
-//   const promises = params.map((cur, index) => new Promise((resolve) => {
-//     resolve(10 * index);
-//   }));
-//   console.log(promises);
-//   return promises;
-// };
-
-// async function runner() {
-//   const result = await Promise.all(getPromises(nums)).then(fulfilled => fulfilled);
-//   console.log(`result---- ${result}`);
-//   console.log(`result++++ ${result}`);
-//   return result;
-// }
-
-// async function getRes() {
-//   console.log(`result^^^^ ${await runner()}`);
-// }
-
-// getRes();
-
-
-/* accurately catch the promise that failed in promise all */
 const getPromises = function (params) {
-  const promises = params.map((cur, index) => new Promise((resolve, reject) => {
-    if ((index + 1) === 2) {
-      reject(new Error(`${index + 1} is failed`));
-    }
-    resolve(10 * (index + 1));
+  const promises = params.map((cur, index) => new Promise((resolve) => {
+    resolve(10 * index);
   }));
+  console.log(promises);
   return promises;
 };
 
 async function runner() {
-  try {
-    const result = await Promise.all(getPromises(nums)).then(fulfilled => fulfilled);
-    console.log(`result---- ${result}`);
-    console.log(`result++++ ${result}`);
-    return result;
-  } catch (err) {
-    return err;
-  }
+  const result = await Promise.all(getPromises(nums)).then(fulfilled => fulfilled);
+  console.log(`result---- ${result}`);
+  console.log(`result++++ ${result}`);
+  return result;
 }
 
 async function getRes() {
@@ -55,6 +26,35 @@ async function getRes() {
 }
 
 getRes();
+
+
+/* accurately catch the promise that failed in promise all */
+// const getPromises = function (params) {
+//   const promises = params.map((cur, index) => new Promise((resolve, reject) => {
+//     if ((index + 1) === 2) {
+//       reject(new Error(`${index + 1} is failed`));
+//     }
+//     resolve(10 * (index + 1));
+//   }));
+//   return promises;
+// };
+
+// async function runner() {
+//   try {
+//     const result = await Promise.all(getPromises(nums)).then(fulfilled => fulfilled);
+//     console.log(`result---- ${result}`);
+//     console.log(`result++++ ${result}`);
+//     return result;
+//   } catch (err) {
+//     return err;
+//   }
+// }
+
+// async function getRes() {
+//   console.log(`result^^^^ ${await runner()}`);
+// }
+
+// getRes();
 
 /* test use promise.all without async/wait, result cannot get the returned value */
 // const nums = [1, 2, 3, 4, 5];
