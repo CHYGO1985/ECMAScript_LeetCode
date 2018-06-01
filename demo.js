db7e71dfbdd03510cf81337b451eb5ddebe7364c
Jingjie Jiang <jingjiejiang85@gmail.com>
2018-06-01 22:01:25 +0800

    update demo.hs

--- demo.js
+++ demo.js
@@ -73,10 +73,10 @@ console.log(JSON.stringify(jsonRes, null, 2));
 // output as title : value ...
 sheetIds.forEach((ele) => {
   const worksheet = workBook.Sheets[ele];
-  const headers = {};
-  const data = {};
+  const headers = [];
+  const data = [];
   // iterate all the keys in worksheet : "!ref"， “A1”，"B1", “A2”, "B2"。。。
-  Object.keys(worksheet).forEach((key) => {
+  Object.keys(worksheet).forEach((key, index) => {
     // get only valid columns start with Capital Letters
     if (key[0] === '!') return;
     // get pos of fst number in key
@@ -86,17 +86,22 @@ sheetIds.forEach((ele) => {
     const col = key.substring(0, colIndex); // get column ID A...
     const row = parseInt(key.substring(colIndex), 10); // get row num 1, 2...
     const value = worksheet[key].v;
+    console.log('col %s row %s value %s', col, row, value);
 
     // store header names
     if (row === 1 && !_.isEmpty(value)) {
-      headers[col] = value;
+      if (_.isEmpty(headers[row - 1])) headers[row - 1] = value;
+      else headers[row] = value;
       return;
     }
 
-    if (!data[headers[col]]) data[headers[col]] = [];
-    data[headers[col]].push(value);
+    // if (_.isNil(data[headers[index]])) data[headers[index]] = [];
+    // data.push(Object.assign({headers[inx], value}));
+    console.log('%j', headers);
+    //console.log('index %d %s %j', index, headers[row - 1], data);
   });
-  jsonRes[ele] = data;
+  jsonRes[ele] = [];
+  jsonRes[ele].push(data);
 });
 
 console.log(JSON.stringify(jsonRes, null, 2));
